33f0328a0f42fc62f11daad01db4cf665a6ad0a7
gaarakseven <naizhuan_huang@163.com>
2019-09-11 15:09:54 +0800

    新闻、通知、资源controller

--- src/main/java/com/gooalgene/wutbiolab/controller/admin/NoticeController.java
+++ src/main/java/com/gooalgene/wutbiolab/controller/admin/NoticeController.java
@@ -1,35 +1,31 @@
 package com.gooalgene.wutbiolab.controller.admin;
 
-import com.gooalgene.wutbiolab.entity.news.NewsDetail;
+import com.gooalgene.wutbiolab.entity.notice.NoticeDetail;
 import com.gooalgene.wutbiolab.response.common.CommonResponse;
 import com.gooalgene.wutbiolab.response.common.PageResponse;
 import com.gooalgene.wutbiolab.response.common.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
-@RequestMapping("/news")
-public class NewsController {
-
+@RequestMapping("/notice")
+public class NoticeController {
     @GetMapping
-    public CommonResponse<PageResponse<NewsDetail>> getNewsDetailList(Integer pageNum, Integer pageSize) {
+    public CommonResponse<PageResponse<NoticeDetail>> getNoticeDetailList(Integer pageNum, Integer pageSize) {
         return ResponseUtil.success(new PageResponse<>());
     }
 
     @GetMapping("/{id}")
-    public CommonResponse<NewsDetail> getNewsDetail(@PathVariable Integer id) {
-        return ResponseUtil.success(new NewsDetail());
+    public CommonResponse<NoticeDetail> getNoticeDetail(@PathVariable Integer id) {
+        return ResponseUtil.success(new NoticeDetail());
     }
 
     @PostMapping
-    public CommonResponse<Boolean> renewNewsDetail(NewsDetail newsDetail) {
+    public CommonResponse<Boolean> renewNoticeDetail(NoticeDetail noticeDetail) {
         return ResponseUtil.success(true);
     }
 
     @DeleteMapping("/{id}")
-    public CommonResponse<Boolean> deleteNewsDetail(@PathVariable Integer id) {
+    public CommonResponse<Boolean> deleteNoticeDetail(@PathVariable Integer id) {
         return ResponseUtil.success(true);
     }
 }
