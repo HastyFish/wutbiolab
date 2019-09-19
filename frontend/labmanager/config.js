a7efb060f165c9d1e0695aac153310d1380cfa32
wangjinyu <wangjinyu@gooalgene.com>
2019-09-19 14:28:59 +0800

    --

--- frontend/labmanager/config.js
+++ frontend/labmanager/config.js
@@ -1,6 +1,6 @@
 const  baseApis = {
   //development:"/",//开发环境地址
-  development:"http://172.168.1.209/wutbiolab",//开发环境地址
+  development:"http://172.168.1.209:83/wutbiolab",//开发环境地址
   test:"https://test-admin.gooalgene.com/officialweb",//测试环境地址
   production:"https://admin.gooalgene.com/officialweb",//正式环境地址
 }
