cecb2178b34f1bbbc5823ee4c49edd911bbf0b87
wangjinyu <wangjinyu@gooalgene.com>
2019-09-19 16:22:36 +0800

    资源模块前端接口

---
+++ src/test/java/com/gooalgene/wutbiolab/ResourceTests.java
@@ -0,0 +1,42 @@
+package com.gooalgene.wutbiolab;
+
+import com.gooalgene.wutbiolab.dao.lab.LabDetailDAO;
+import com.gooalgene.wutbiolab.dao.resource.ResourceDetailDAO;
+import com.gooalgene.wutbiolab.entity.lab.LabDetail;
+import com.gooalgene.wutbiolab.entity.resource.ResourceDetail;
+import com.gooalgene.wutbiolab.entity.resource.ResourceOverview;
+import com.gooalgene.wutbiolab.service.LabService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourceTests {
+	@Autowired
+	private ResourceDetailDAO resourceDetailDAO;
+
+	@Test
+	public void testDAO() {
+		Page<ResourceOverview> newsDetailByPublishStatus = resourceDetailDAO.findNewsDetailByPublishStatus(1, PageRequest.of(0, 11));
+
+		newsDetailByPublishStatus.forEach(resourceOverview -> {
+			Long id = resourceOverview.getId();
+			Long publishDate = resourceOverview.getPublishDate();
+			String title = resourceOverview.getTitle();
+			System.out.println(id+":"+title+":"+publishDate);
+		});
+		System.out.println(1);
+
+	}
+
+
+
+}
