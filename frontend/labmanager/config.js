51981b3f00713d96730275f0113b5ee28c4874aa
wangjinyu <wangjinyu@gooalgene.com>
2019-09-26 14:41:14 +0800

    生产环境配置修改

--- frontend/labmanager/config.js
+++ frontend/labmanager/config.js
@@ -2,6 +2,6 @@ const  baseApis = {
   //development:"/",//开发环境地址
   development:"http://172.168.1.209:83/wutbiolab",//开发环境地址
   test:"http://wutbiolabadmin.gooalgene.com/wutbiolab",//测试环境地址
-  production:"https://wutadmin.gooalgene.com/wutbiolab",//正式环境地址
+  production:"http://wutadmin.gooalgene.com/wutbiolab",//正式环境地址
 }
 module.exports = baseApis
