f52292ea2ee09a558306859d14ec989632af5fa1
王勇 <wangyong@gooalgene.com>
2019-09-29 17:37:01 +0800

    Resources

--- frontend/labweb/src/views/resources/resources.jsx
+++ frontend/labweb/src/views/resources/resources.jsx
@@ -15,7 +15,7 @@ class Resources extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            titleinfo : NavTitle[4].en,
+            titleinfo : NavTitle[5].en,
             navList : [],
             dataList:[],
             navId:37,
